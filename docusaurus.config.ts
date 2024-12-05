d103781444b52cdf8f181a5488a652a4da6d5713
Kamran <kamranv21@gmail.com>
2024-12-06 01:26:26 +0400

    config update

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,15 +10,17 @@ const config: Config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://teklifo.github.io/",
+  url: "https://github.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/documentation",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "teklifo", // Usually your GitHub org/user name.
   projectName: "teklifo", // Usually your repo name.
+  deploymentBranch: "gh-pages",
+  trailingSlash: false,
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
